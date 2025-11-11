191ebf2a4d79c707c6f9ceea785bc8796b75c815
Cory Kaufman-Schofield <cory@corykaufman.com>
2025-11-11 13:54:47 -0500

    Update image

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
-FROM heroku/log-shuttle:0.14.0
+FROM heroku/log-shuttle:0.23.0-1-gabfbbf9
 
 RUN apk update
 
-RUN apk-install wget sudo bash socat
+RUN apk add wget sudo bash socat
 
 ADD ./heroku_kinesis.sh /root/
 
 EXPOSE 514/udp
 
-ENTRYPOINT ["/bin/bash"]
+ENTRYPOINT ["/bin/bash", "-c"]
 CMD ["/root/heroku_kinesis.sh"]
--- heroku_kinesis.sh
+++ heroku_kinesis.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-socat -u udp-recv:514 - | /bin/log-shuttle -logs-url $KINESIS_URL -max-line-length 35000 -batch-size 150 -input-format rfc5424 -kinesis-shards $KINESIS_SHARDS
+
+env | grep KINESIS
+
+socat tcp4-listen:514,fork - | /bin/log-shuttle \
+  -logs-url "$KINESIS_URL" \
+  -max-line-length 32000 \
+  -batch-size 150 \
+  -input-format rfc5424 \
+  -kinesis-shards "$KINESIS_SHARDS" \
+  -kinesis-parition-key "$KINESIS_PARTITION_KEY" \
+  -verbose
